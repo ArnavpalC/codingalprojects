41ebd909dbfe1bb33982db59932770ab6ccc7956
ArnavpalC <arnavpalchoudhury@gmail.com>
2025-12-23 15:28:57 +0530

    Add files via upload

---
+++ number swap.py
@@ -0,0 +1,8 @@
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+c = int(input("Enter third number: "))
+
+a, b, c = c, a, b
+
+print("After switching:")
+print(a, b, c)
