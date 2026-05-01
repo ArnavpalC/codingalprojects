140a943d8cb607ad1d57b62da668710810f1f509
ArnavpalC <arnavpalchoudhury@gmail.com>
2026-05-01 20:36:36 +0530

    Add files via upload

---
+++ 1's.py
@@ -0,0 +1,20 @@
+def max_consecutive_ones(n):
+    binary = bin(n)[2:]
+    
+    max_count = 0
+    current_count = 0
+    
+    for bit in binary:
+        if bit == '1':
+            current_count += 1
+            max_count = max(max_count, current_count)
+        else:
+            current_count = 0
+    
+    return max_count
+
+
+num = int(input("Enter your number: "))
+result = max_consecutive_ones(num)
+
+print("Longest consecutive 1’s length :", result)
