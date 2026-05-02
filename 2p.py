3ec6631056099535de0a31b1ed7d3fe94526e85d
ArnavpalC <arnavpalchoudhury@gmail.com>
2026-05-02 10:02:57 +0530

    Add files via upload

---
+++ 2p.py
@@ -0,0 +1,22 @@
+def is_prime(n):
+
+    if n < 2:
+        return False
+
+
+    if (n & 1) == 0:
+        return n == 2
+
+
+    i = 3
+    while i * i <= n:
+        if n % i == 0:
+            return False
+        i += 2
+
+
+    return True
+
+for num in range(10, 100):
+    if is_prime(num):
+        print(num, end=" ")
